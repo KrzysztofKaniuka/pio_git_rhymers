38e7de6423e0be1eaf65316641224788833fa4fe
KrzysztofKaniuka <240697@edu.p.lodz.pl>
2023-05-10 15:25:52 +0200

    13. Dodanie metody testRhymers która przejmuje większość funkcji metody main

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -7,27 +7,31 @@ class RhymersDemo {
 
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
-        
+
+        testRhymers(factory);
+
+    }
+
+    private static void testRhymers(Rhymersfactory factory) {
         DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFifoRhymer(), factory.getHanoiRhymer()};
-        
+
         for (int externalIterator = 1; externalIterator < 15; externalIterator++)
             for (int internalIterator = 0; internalIterator < 3; internalIterator++)
                 rhymers[internalIterator].countIn(externalIterator);
-        
+
         java.util.Random rn = new java.util.Random();
         for (int iterator = 1; iterator < 15; iterator++)
             rhymers[3].countIn(rn.nextInt(20));
-        
+
         for (int iterator = 0; iterator < rhymers.length; iterator++) {
             while (!rhymers[iterator].callCheck())
                 System.out.print(rhymers[iterator].countOut() + "  ");
             System.out.println();
         }
-        
+
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[3]).reportRejected());
-        
     }
-    
+
 }
